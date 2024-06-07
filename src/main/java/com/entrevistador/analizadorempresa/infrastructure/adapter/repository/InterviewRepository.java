41569499945c5ea782fab73072266c5eb95aad51
Kirenai <pool_dota2@hotmail.com>
2024-06-06 23:39:12 -0500

    Error timeout connecting

--- src/main/java/com/entrevistador/analizadorempresa/infrastructure/adapter/repository/InterviewRepository.java
+++ src/main/java/com/entrevistador/analizadorempresa/infrastructure/adapter/repository/InterviewRepository.java
@@ -2,6 +2,8 @@ package com.entrevistador.analizadorempresa.infrastructure.adapter.repository;
 
 import com.entrevistador.analizadorempresa.infrastructure.adapter.entity.InterviewEntity;
 import org.springframework.data.elasticsearch.repository.ReactiveElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InterviewRepository extends ReactiveElasticsearchRepository<InterviewEntity, String> {
 }
--- src/main/java/com/entrevistador/analizadorempresa/infrastructure/beanconfiguration/ElasticConfiguration.java
+++ src/main/java/com/entrevistador/analizadorempresa/infrastructure/beanconfiguration/ElasticConfiguration.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ReactiveElasticsearchConfiguration;
 
+import java.time.Duration;
+
 @Configuration
 public class ElasticConfiguration extends ReactiveElasticsearchConfiguration {
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-                .connectedTo("https://elasticsearch.pruebas-entrevistador-inteligente.site:443")
+                .connectedTo("elasticsearch.pruebas-entrevistador-inteligente.site:9200")
+                .withConnectTimeout(Duration.ofSeconds(5))
                 .build();
     }
 }
