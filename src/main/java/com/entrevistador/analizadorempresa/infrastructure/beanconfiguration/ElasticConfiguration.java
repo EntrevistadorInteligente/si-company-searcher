09207785073c6d1262a3c895b6d6257bf4ffacbe
JamiltonQuintero <transportador1118@gmail.com>
2024-10-04 20:41:46 -0500

    Se eagrega Elastic con contraseña

--- src/main/java/com/entrevistador/analizadorempresa/infrastructure/beanconfiguration/ElasticConfiguration.java
+++ src/main/java/com/entrevistador/analizadorempresa/infrastructure/beanconfiguration/ElasticConfiguration.java
@@ -4,38 +4,35 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
-import co.elastic.clients.util.ContentType;
-import org.apache.http.HttpHeaders;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 
 @Configuration
 public class ElasticConfiguration {
 
     @Value("${elasticsearch.url}")
     private String elasticsearchUrl;
+    @Value("${elasticsearch.user}")
+    private String elasticsearchUser;
+    @Value("${elasticsearch.pass}")
+    private String elasticsearchPass;
 
     @Bean
     public RestClient getRestClient() {
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("", ""));
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(elasticsearchUser, elasticsearchPass));
         return RestClient.builder(new HttpHost(elasticsearchUrl, 443, "https"))
                 .setHttpClientConfigCallback(httpClientBuilder -> {
                     httpClientBuilder.disableAuthCaching();
-                    httpClientBuilder.setDefaultHeaders(List.of(
-                            new BasicHeader(
-                                    HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON)));
                     httpClientBuilder.addInterceptorLast((HttpResponseInterceptor)
                             (response, context) ->
                                     response.addHeader("X-Elastic-Product", "Elasticsearch"));
